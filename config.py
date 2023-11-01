4057bbd3c36a47e9b80a403a24553ed05a93bc2b
Ethan Martin <60288171+yuckdevchan@users.noreply.github.com>
2023-11-01 23:39:04 +0000

    Add comment to config

--- config.py
+++ config.py
@@ -1,1 +1,2 @@
+# Specify where your Music is. The Music directory should be populated with Artist folders with Album folders inside them and songs inside the Album folders.
 MUSIC_DIR = "/home/deck/Music"
