fb8118774c78c9a27421100b804f90705edc47b1
Ethan Martin <60288171+yuckdevchan@users.noreply.github.com>
2023-11-23 08:50:14 +0000

    some changes

--- config.py
+++ config.py
@@ -3,7 +3,7 @@ from dataclasses import dataclass
 # Choose your desired options below.
 @dataclass
 class UserOptions:
-    MUSIC_DIR = "/home/deck/git/sonic-website/music"
+    MUSIC_DIR = "/home/ethan/Music/"
     CAL_DIR = ".cal"
     PLAYLIST_EXTENSION = ".tardis"
     SCAN_COLLECTION_ON_STARTUP = True
