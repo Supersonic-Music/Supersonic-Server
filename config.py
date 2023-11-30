1b43535675413af788501e73e8ecae85066b3b19
Ethan Martin <60288171+yuckdevchan@users.noreply.github.com>
2023-11-30 18:23:10 +0000

    Change lightyear path to be inside .cal

--- config.py
+++ config.py
@@ -1,11 +1,12 @@
 from dataclasses import dataclass
+from os import path
 
 # Choose your desired options below.
 @dataclass
 class UserOptions:
     MUSIC_DIR = "/home/deck/Music/"
     CAL_DIR = ".cal"
-    LIGHTYEAR_PATH = "lightyear.txt"
+    LIGHTYEAR_PATH = path.join(MUSIC_DIR, CAL_DIR, "meta", "lightyear.txt")
     PLAYLIST_EXTENSION = ".tardis"
     SCAN_COLLECTION_ON_STARTUP = True
     
