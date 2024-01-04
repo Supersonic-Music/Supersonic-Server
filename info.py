c95015653fd149ae68640628c28009aa1e49cb41
Ethan Martin <60288171+yuckdevchan@users.noreply.github.com>
2024-01-04 15:28:13 +0000

    Update version string

--- info.py
+++ info.py
@@ -5,5 +5,5 @@ import pwd, os
 @dataclass
 class ProgramData:
     PROGRAM_NAME: str = "Supersonic Server"
-    PROGRAM_VERSION: str = "0.idk"
+    PROGRAM_VERSION: str = "v1.2.1"
     auto_user_music = f"/home/{pwd.getpwuid(os.getuid())[0]}/Music"
